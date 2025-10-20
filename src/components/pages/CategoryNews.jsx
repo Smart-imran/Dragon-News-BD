bcd2143f4276ff84631e5c43a30513696be7a03e
Smart-imran <rst.imran4@gmail.com>
2025-10-21 00:57:55 +0600

    categoryNews

--- src/components/pages/CategoryNews.jsx
+++ src/components/pages/CategoryNews.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const {id}= useParams();
+    console.log(id)
+        
     return (
+        
         <div>
-            CategoryNews
+            CategoryNews {id}
         </div>
     );
 };
